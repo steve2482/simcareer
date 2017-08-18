cd075552c127ba344583d9f3524bd341bd49fbb3
steve2482 <stevecb12684@yahoo.com>
2017-08-17 23:26:41 -0400

    contracts page updated to select teir 1 road if not selected

--- src/js/components/Contracts.js
+++ src/js/components/Contracts.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Button, Col, Grid, Panel, Row, Thumbnail } from 'react-bootstrap';
+
+import mazdaMx5Img from '../../assets/images/2016mazda-mx5.jpg';
+import specFordImg from '../../assets/images/spec-ford.jpg';
+import solsticeImg from '../../assets/images/pontiac-solstice.jpg';
 
 export class Contracts extends React.Component {
   constructor(props) {
@@ -7,7 +12,91 @@ export class Contracts extends React.Component {
   }
 
   render() {
-    return(
+    const user = this.props.state.user;
+    const seriesHeader = (
+      <h3>Series</h3>
+    );
+    const carheader = (
+      <h3>Car</h3>
+    );
+    const pathHeader = (
+      <h3>Where this can take you</h3>
+    );
+
+    // If user is road racing and needs to select thier first contract
+    if (user.path === 'road' && !user.currentCar && !user.currentTier) {
+      return (
+        <div className='center'>
+          <div>
+            <h1>Select your first contract</h1>
+            <p>Can you believe it? You haven't even hit the track, and teams from 3 different series are offering you contracts to drive. You better not make fools of them!</p>
+          </div>
+          <div>
+            <Grid>
+                <Row>
+                  <Col md={3}>
+                    <Thumbnail src={mazdaMx5Img} alt='Mazda MX-5'>
+                      <Panel header={seriesHeader} bsStyle='primary'>
+                        <p>Global Mazda MX-5 Cup Series</p>
+                      </Panel>
+                      <Panel header={carheader} bsStyle='primary'>
+                        <p>Global Mazda MX-5 Cup Car</p>
+                      </Panel>
+                      <Panel header={pathHeader} bsStyle='primary'>
+                        <p>From the Global Mazda MX-5 Cup Series you can progress on to open wheel formula style cars or to 'tin top' sports cars.</p>
+                      </Panel>
+                      <Button bsStyle='primary'>Select</Button>
+                    </Thumbnail>
+                  </Col>
+                  <Col md={3}>
+                    <Thumbnail src={specFordImg} alt='Spec Racer Ford'>
+                      <Panel header={seriesHeader} bsStyle='primary'>
+                        <p>Spec Racer Ford Challenge</p>
+                      </Panel>
+                      <Panel header={carheader} bsStyle='primary'>
+                        <p>SCCA Spec Racer Ford</p>
+                      </Panel>
+                      <Panel header={pathHeader} bsStyle='primary'>
+                        <p>From the Spec Racer For Challenge you can progress on to 'tin top' sports cars.</p>
+                      </Panel>
+                      <Button bsStyle='primary'>Select</Button>
+                    </Thumbnail>
+                  </Col>
+                  <Col md={3}>
+                    <Thumbnail src={solsticeImg} alt='Pontiac Solstice'>
+                      <Panel header={seriesHeader} bsStyle='primary'>
+                        <p>Production Car Challenge</p>
+                      </Panel>
+                      <Panel header={carheader} bsStyle='primary'>
+                        <p>Pontiac Solstice</p>
+                      </Panel>
+                      <Panel header={pathHeader} bsStyle='primary'>
+                        <p>From the Production Car Challenge you can progress on 'tin top' sports cars.</p>
+                      </Panel>
+                      <Button bsStyle='primary'>Select</Button>
+                    </Thumbnail>
+                  </Col>
+                  <Col md={3}>
+                    <Thumbnail src={mazdaMx5Img} alt='Mazda MX-5'>
+                      <Panel header={seriesHeader} bsStyle='primary'>
+                        <p>Production Car Challenge</p>
+                      </Panel>
+                      <Panel header={carheader} bsStyle='primary'>
+                        <p>Global Mazda MX-5 Cup Car</p>
+                      </Panel>
+                      <Panel header={pathHeader} bsStyle='primary'>
+                        <p>From the Production Car Challenge you can progress on 'tin top' sports cars.</p>
+                      </Panel>
+                      <Button bsStyle='primary'>Select</Button>
+                    </Thumbnail>
+                  </Col>
+                </Row>
+              </Grid>
+          </div>
+        </div>
+      )
+    }
+    return (
       <h1>This is the User's Contracts'</h1>
     );
   }
